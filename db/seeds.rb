102f2b33adc1a499e32c8ce36477748a396da3ef
Nishant Kiran <nishantkiran92@gmail.com>
2014-12-06 16:16:15 +0530

    todo app commited with updated gem file for heroku

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Todo.create!(title: 'Configure Omniauth', notes: 'study passthru error, verify controller code')
+Todo.create!(title: 'Explore different Bootstraps')
+Todo.create!(title: 'Clean up Hard drive', notes: 'Lot of bloatware and registry cleanup')
+Todo.create!(title: 'Game hunt', notes: 'New releases on Black Friday!!')
+Todo.create!(title: 'Party Saturday', notes: 'Tshirts in Laundry')
